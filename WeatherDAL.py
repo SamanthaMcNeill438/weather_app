f8ef3161ec79ea8f47264d0f4d8f9015de948753
Samantha <s.c.mcalister03@gmail.com>
2024-07-05 11:26:41 +0100

    DBConnection UPDATES

--- WeatherDAL.py
+++ WeatherDAL.py
@@ -12,4 +12,7 @@ class WeatherDAL:
         # create add data functuion
         # create delete data function
         # create update function    
-        #  create searches/queries
+        # create searches/queries
+
+    # DATABASE QUERIES
+    cur.execute("SELECT Min_Temp, Max_Temp, Max_Temp, Max_Temp FROM Weather WHERE City == City")
--- mainsql.sql
+++ mainsql.sql
@@ -9,8 +9,8 @@ CREATE TABLE Weather(
     City nvarchar(50) not null,
     Min_Temp float not null,
     Max_Temp float not null,
-    Avg_Temp float not null, 
-    Humidity float not null,
+    Max_Temp float not null, 
+    Max_Temp float not null,
     CONSTRAINT [PK_City] PRIMARY KEY ([City])
 )
 
