ea9dea6da0dcfbdcf357e3fe38a9d98919543c04
Samantha <s.c.mcalister03@gmail.com>
2024-07-05 09:21:42 +0100

    Test updates

--- main.py
+++ main.py
@@ -5,13 +5,51 @@ from fastapi import FastAPI, HTTPException
 import json
 import requests
 import config
-import WeatherData
+import WeatherData 
+
+class WeatherData:
+    #city: str
+    min_temp = 0
+    max_temp = 0
+    avg_temp = 0
+    humidity = 0
+
+    def __init__(self, min_temp, max_temp, humidity):
+        #self.city = city
+        self.min_temp = min_temp
+        self.max_temp = max_temp
+        self.avg_temp = round(((min_temp + max_temp)/2),2)
+        self.humidity = humidity
+    
+
+    def get_weather_data(self):
+        #     """
+        # Extracts the weather data from the API response.
+
+        # Args:
+        #     weather_data (dict): The API response data.
+
+        # Returns:
+        #     dict: A dictionary with keys "min temp:", "max temp:", "avg temp:", and "humidity:".
+
+        # Example:
+        #     >>> weather_data = {"main": {"temp_min": 10, "temp_max": 20, "humidity": 60}}
+        #     >>> get_data(weather_data)
+        #     {"min temp:": 10, "max temp:": 20, "avg temp:": 15.0, "humidity:": 60}
+        # """
+        return {
+            "min temp:": self.min_temp,
+            "max temp:": self.max_temp,
+            "avg temp:": self.avg_temp,
+            "humidity:": self.humidity
+        }
+
+
+    
 
 app = FastAPI()
 API_KEY = config.get_API_KEY()
 units = config.get_units()
-
-
 
 @app.get("/")
 def read_root():
@@ -50,7 +88,7 @@ async def get_weather(city: str):
     print(weather_data)
 
     if weather_data["cod"] != "200": 
-        HTTPException(status_code=weather_data, detail='An error occured')
+        #HTTPException(status_code=weather_data, detail='An error occured')
         return {"City not found"}
         #ßprint("stopped working")
     return get_data(weather_data)
@@ -64,7 +102,7 @@ def get_data(weather_data):
     humidity = weather_data["main"]["humidity"]
     
     data = WeatherData(min_temp, max_temp, humidity)
-    return data.get_weather_data()
+    return data.get_weather_data(data)
     # return {
     #     "min temp:": min_temp,
     #     "max temp:": max_temp,
--- tests/tests.py
+++ tests/tests.py
@@ -11,59 +11,68 @@ import config
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+# def test_read_main():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"msg": "Hello World"}
 
 
-# @pytest.fixture
-# def weather_fixture():
+# # @pytest.fixture
+# # def weather_fixture():
+# #     city = "Belfast"
+
+
+# def test_get_weather(weather_fixture):
 #     city = "Belfast"
+#     response = client.get("/weather/{city}")
+#     assert response.status_code == 200
+#     {
+#         # "min temp:": main.WeatherData.min_temp,
+#         # "max temp:": main.WeatherData.max_temp,
+#         # "avg temp:": main.WeatherData.avg_temp,
+#         # "humidity:": main.WeatherData.humidity
+#         "min temp:": main.data.min_temp,
+#         "max temp:": main.data.max_temp,
+#         "avg temp:": main.data.avg_temp,
+#         "humidity:": main.data.humidity
+#     }
 
-
-def test_get_weather(weather_fixture):
-    city = "Belfast"
-    response = client.get("/weather/{city}")
-    assert response.status_code == 200
-    {
-        # "min temp:": main.WeatherData.min_temp,
-        # "max temp:": main.WeatherData.max_temp,
-        # "avg temp:": main.WeatherData.avg_temp,
-        # "humidity:": main.WeatherData.humidity
-        "min temp:": main.data.min_temp,
-        "max temp:": main.data.max_temp,
-        "avg temp:": main.data.avg_temp,
-        "humidity:": main.data.humidity
-    }
-
-def test_weather_class():
-    data: object
-    data = main.WeatherData(10,20,44)
-    response = data.get_weather_data()
-    assert response == {
-            "min temp:": 10,
-            "max temp:": 20,
-            "avg temp:": 15.00,
-            "humidity:": 44 }
+# def test_weather_class():
+#     data: object
+#     data = main.WeatherData(10,20,44)
+#     response = data.get_weather_data()
+#     assert response == {
+#             "min temp:": 10,
+#             "max temp:": 20,
+#             "avg temp:": 15.00,
+#             "humidity:": 44 }
     
 
-def test_link():
-    API_KEY = config.get_API_KEY()
-    units = config.get_units()
-    city = "London"  # Replace with the desired city
+# def test_link():
+#     API_KEY = config.get_API_KEY()
+#     units = config.get_units()
+#     city = "London"  # Replace with the desired city
+#     base_url = "https://api.openweathermap.org/data/2.5/weather"
+#     params = {
+#         "q": city,
+#         "units": units,  
+#         "appid": API_KEY
+#     }
+
+#     response = requests.get(base_url, params=params)
+#     assert response.status_code == 200  # Check if the response was successful
+
+#     # weather_data = response.json()  # Use the response.json() method to parse the JSON response
+#     # Do something with the weather_data
+
+
+def test_get_weather():
     base_url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
-        "q": city,
-        "units": units,  
-        "appid": API_KEY
+        "q": 'Belfast',
+        "units": config.get_units(),  
+        "appid": config.get_API_KEY()
     }
-
     response = requests.get(base_url, params=params)
-    assert response.status_code == 200  # Check if the response was successful
-
-    # weather_data = response.json()  # Use the response.json() method to parse the JSON response
-    # Do something with the weather_data
-
-
-test_link()
+    assert response.status_code == 200
+    assert response.json()["main"]["humidity"] == 62
